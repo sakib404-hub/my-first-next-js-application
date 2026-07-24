21f6be54dc77d641aaa9b751a2a3dc65e5ae317f
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-24 15:50:49 +0600

    route grouping with parenthesis

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      This is the register page!
+    </div>
+  )
+}
+
+export default RegisterPage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import Link from "next/link";
 import LikeButton from "./ui/likeButton";
 
-const page = () => {
+const page = async() => {
   //? console.log("This is the root route!"); this will be shown in the terminal as it is a server side component
+
+  const post = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+  const postData = await post.json();
+
+  // console.log(postData);
 
   return (
     <div>
