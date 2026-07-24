177e82ef5fc783a2e7ef7c6b05b7a6360f7447e9
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-24 21:54:20 +0600

    revalidating cache

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import Link from "next/link";
 import LikeButton from "./ui/likeButton";
 import { getBlog } from "./services/getBlog";
+import { cacheLife, cacheTag } from "next/cache";
 
 const page = async () => {
   //? console.log("This is the root route!"); this will be shown in the terminal as it is a server side component
+  "use cache";
+  cacheLife("hours");
+  cacheTag("blogs");
 
   const blogs = await getBlog();
 
--- app/services/getBlog.ts
+++ app/services/getBlog.ts
@@ -1,12 +1,24 @@
+import { revalidateTag } from "next/cache";
 import { cacheLife } from "next/cache";
 
 
 export const getBlog = async () => {
-    "use cache";
-    cacheLife("hours");
-    
-    const post = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const post = await fetch("https://jsonplaceholder.typicode.com/todos", {
+        method : "GET",
+        cache : "force-cache",
+        next : {
+            revalidate : 60 * 60 * 24,
+            tags : ["blogs"]
+        }
+    });
 
     const postData = await post.json();
     return postData;
+}
+
+export const renewBlogCache = ()=> {
+    // revalidateTag("blogs", "max");
+    revalidateTag("blogs", {
+        expire : 60 * 60 * 24 * 7
+    });
 }
