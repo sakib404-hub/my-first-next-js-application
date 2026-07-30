fcc8bb36ff988ece637326276a5bac45ae2dab6f
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-30 19:30:16 +0600

    handling logged in user login and register route

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,55 @@
+import  jwt, { JwtPayload }  from 'jsonwebtoken';
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server'
 
-export const proxy = (request : NextRequest) => {
-  return NextResponse.redirect(new URL('/', request.url))
+const AUTH_ROUTE = [
+    "/login",
+    "/register"
+]
+
+export const proxy = async(request : NextRequest) => {
+    const pathname = request.nextUrl.pathname;
+    // console.log(request.nextUrl.pathname);
+    // return NextResponse.redirect(new URL('/', request.url))
+
+    //? this is one way of getting t he access token 
+    // const cookieStore = await cookies();
+    // const accessToken = cookieStore.get("accessToken")?.value;
+
+    //? another way of getting the access token
+    const accessToken = request.cookies.get("accessToken")?.value;
+
+    const decoded = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+
+    let userRole = null;
+
+    if(decoded){
+        userRole = decoded.role
+    }
+
+    //? when the user is logged in and trying to get to the login or the register route we will redirect him to the home route or dashboard
+    if(accessToken && AUTH_ROUTE.includes(pathname)){
+        if(userRole === "USER"){
+            return NextResponse.redirect(new URL("/dashboard", request.url));
+        }else if(userRole === "ADMIN"){
+             return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+        }else if(userRole === "AUTHOR"){
+             return NextResponse.redirect(new URL("/author-dashboard", request.url));
+        }else {
+             return NextResponse.redirect(new URL("/", request.url));
+        }
+    }
+
+
+    return NextResponse.next();
 }
 
+//? if matcher is given with regular expression therefore it is known as catcher of all route
 export const config = {
     matcher : [
-        "/dashboard/:path*",
-        "/admin-dashboard/:path*"
+        // "/dashboard/:path*",
+        // "/admin-dashboard/:path*"
+        '/((?!api|_next/static|_next/image|.*\\.png$).*)'
     ]
 
 }
