9d0de711fbb56ca84247087e0200c180dbf7b3d6
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-30 20:44:29 +0600

    added refresh Token

---
+++ services/refreshToken.ts
@@ -0,0 +1,28 @@
+"use server"
+
+import { cookies } from "next/headers"
+
+export const refreshToken = async()=>{
+    const cookieStrore = await cookies();
+
+   const refreshToken = cookieStrore.get("refreshToken")?.value;
+
+    if(!refreshToken){
+        return {
+            success : false, 
+            message : "User Not Logged In."
+        }
+    }
+    
+    const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/refresh-token`,{
+        method : "POST",
+        headers : {
+            Authorization : `${refreshToken}`,
+            Cookie : `refreshToken=${refreshToken}`
+        },
+        cache : "no-store",
+    });
+
+    const result = await res.json();
+    return result;
+}
